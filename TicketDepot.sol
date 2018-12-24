91344d297d81112c0a17b4b14a0ba441cea2266a
Павел <44447864+paul178sh@users.noreply.github.com>
2018-12-24 11:57:08 +0300

    Update TicketDepot.sol

--- TicketDepot.sol
+++ TicketDepot.sol
@@ -16,16 +16,26 @@ contract TicketDepot {
 
    function ticketDepot(uint64 _transactionFee) {
        // Конструктор конктракта. Устанавливает transactionFee и owner
-       owner = msg.sender;
        transactionFee = _transactionFee;
+       owner = msg.sender;
+       numEvents = 0;
    }
    
    function createEvent(uint64 _ticketPrice, uint16 _ticketsAvailable) returns (uint16 eventID) {
        // Создает мероприятие с _ticketsAvailable билетами по цене _ticketPrice, а также устанавливает owner мероприятия
+       eventID = numEvents;
+       events[eventID] = Event(owner, _ticketPrice, _ticketsAvailable);
+       numEvents++;
    }
    
    function buyNewTicket(uint16 _eventID, address _attendee) payable returns (uint16 ticketID) {
        // Позволяет купить билет: если послано достаточно денег, чтобы оплатить комиссию владельца контракта + сам билет,
        // то _attendee в attendees соответствующего event. Уменьшает количество доступных билетов.
        // Сразу переводит деньги owner мероприятия.
+       if (events[_eventID].ticketsRemaining > 0 && msg.value >events[_eventID].ticketPrice + transactionFee){
+       events[_eventID].attendees[ticketID] = _attendee;
+       ticketID = events[_eventID].ticketsRemaining--;
+       events[_eventID].owner.transfer(msg.value - transactionFee);
    }
+ }
+}
